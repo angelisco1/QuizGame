4eddac5e2b8b51392a3565b79855139d2a7a7a5b
Angel Villalba <angelvil92@gmail.com>
2015-07-24 15:05:57 +0200

    Puntuation system is working

--- quiz.js
+++ quiz.js
@@ -1,13 +1,15 @@
 var read = require('read');
 
 
-var Question = function(qText, qAnswer, qId){
+var Question = function(qText, qAnswer, qId, qPoint){
 	this.qText = qText;
 	this.qAnswer = qAnswer;
 	this.qId = qId;
+	this.qPoint = qPoint;
+	this.qBonus = false;
 
 	this.showQuestion = function(){
-		console.log("\n------------------\n# " + this.qId + "\n" + this.qText + "\n");
+		console.log("\n------------------\n# " + this.qId + "\n" + this.qText + "\nPoints: " + this.qPoint + "\n");
 	}
 
 	this.isCorrect = function(answer){
@@ -23,9 +25,34 @@ var Quiz = function(){
 
 	this.questionList = [];
 	this.i = 0;
+	this.totalScore = 0;
 
-	this.addQuestion = function(qText, qAnswer){
-		question = new Question(qText, qAnswer, this.getLastId());
+	this.startGame = function(){
+		this.selectBonusQuestion();
+		this.game();
+	}
+
+	this.game = function(){
+		if(this.i < this.questionList.length){
+			console.log("You have " + this.totalScore + " points.");
+			this.questionList[this.i].showQuestion();
+			if(this.questionList[this.i].qBonus){
+				console.log("This is a bonus question..." + 
+					"If you answer correctly, you'll get double points." + 
+					"Good luck!!!");
+			}
+			options = {
+				prompt: "Introduzca su respuesta...\n > "
+			};				
+			read(options, this.play.bind(this));
+		}else{
+			console.log("You have got a total score of " + this.totalScore + " points.");
+			console.log("There are not more questions...");
+		}
+	}
+
+	this.addQuestion = function(qText, qAnswer, qPoint){
+		question = new Question(qText, qAnswer, this.getLastId(), qPoint);
 		this.questionList.push(question);
 	}
 
@@ -43,17 +70,11 @@ var Quiz = function(){
 		return question.isCorrect(userAnswer);
 	}
 
-	this.game = function(){
-			if(this.i < this.questionList.length){
-				this.questionList[this.i].showQuestion();
-				options = {
-					prompt: "Introduzca su respuesta...\n > "
-				};				
-				read(options, this.play.bind(this));
-			}else{
-				console.log("There are not more questions...");
-			}
+	this.selectBonusQuestion = function(){
+		var randomId = Math.floor(Math.random()*this.questionList.length);
+		this.questionList[randomId].qBonus = true;
 	}
+
 
 	this.play = function(err, userAnswer){
 		if(err){
@@ -63,8 +84,16 @@ var Quiz = function(){
 		var question = this.questionList[this.i];
 		if(this.checkAnswer(userAnswer, question)){
 			this.i++;
-			console.log("Your answer is correct...");
+			var points = question.qPoint;
+			var strCorrect = "Your answer is correct...";
+			if(question.qBonus){
+				points *= 2;
+				strCorrect += "You get double points.";
+			}
+			this.totalScore += points;
+			console.log(strCorrect);
 		}else{
+			this.totalScore -= question.qPoint;
 			console.log("Your answer is incorrect :(... Try again");
 		}
 		this.game();
@@ -72,12 +101,15 @@ var Quiz = function(){
 
 }
 
+
 quiz = new Quiz();
 
-quiz.addQuestion("How many hearts have an octopus??", "3");
-quiz.addQuestion("Which element is heaviest, Mg or Mn??", "Mn");
-quiz.addQuestion("Who sang 'My Way'??", "Frank Sinatra");
-quiz.addQuestion("Where are the Dolomites??", "Italy");
-quiz.addQuestion("What is the capital of Australia??", "Canberra");
+quiz.addQuestion("How many hearts have an octopus??", "3", 5);
+quiz.addQuestion("Which element is heaviest, Mg or Mn??", "Mn", 6);
+quiz.addQuestion("Who sang 'My Way'??", "Frank Sinatra", 6);
+quiz.addQuestion("Where are the Dolomites??", "Italy", 5);
+quiz.addQuestion("What is the capital of Australia??", "Canberra", 5);
+quiz.addQuestion("How many months have 31 days??", "7", 3);
+quiz.addQuestion("Who invented the telephone??", "Bell", 5);
 
-quiz.game();
+quiz.startGame();
