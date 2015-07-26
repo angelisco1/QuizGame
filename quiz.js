e236b1aac869690a0620aee3becd3de34a5bb7ef
Angel Villalba <angelvil92@gmail.com>
2015-07-26 14:58:47 +0200

    Added class User, now it is possible to create a new user or log in with an existing user.

--- quiz.js
+++ quiz.js
@@ -13,7 +13,7 @@ var Question = function(qText, qAnswer, qId, qPoint){
 	}
 
 	this.isCorrect = function(answer){
-		if(this.qAnswer === answer){
+		if(this.qAnswer === answer.toLowerCase()){
 			return true;
 		}
 		return false;
@@ -24,17 +24,98 @@ var Question = function(qText, qAnswer, qId, qPoint){
 var Quiz = function(){
 
 	this.questionList = [];
+	this.userList = [];
 	this.i = 0;
-	this.totalScore = 0;
+	// this.totalScore = 0;
+	this.currentUser = null;
 
 	this.startGame = function(){
 		this.selectBonusQuestion();
-		this.game();
+		this.getUser();
+		// if(this.currentUser !== null){
+		// 	console.log(this.currentUser);
+		// 	this.game();
+		// }else{
+		// 	console.log("FINITO");			
+		// }
+
+	}
+
+	this.getUser = function(){
+		options = {
+			prompt: "Are you registered??\n>"
+		};				
+		read(options, this.exitsUser.bind(this));
+	}
+
+	this.exitsUser = function(err, newUser){
+		if(err){
+			throw err;
+		}
+		switch(newUser){
+			case "no":
+				this.register();
+				break;
+			case "yes":
+				this.login();
+				break;
+			default:
+				this.getUser();
+		}
+	}
+
+	this.login = function(){
+		console.log("LOGIN:");
+		options = {
+			prompt: "Username: \n>"
+		};				
+		read(options, this.loadUser.bind(this));
+	}
+
+	this.register = function(){
+		console.log("REGISTER:");
+		options = {
+			prompt: "New username: \n>"
+		};				
+		read(options, this.createUser.bind(this));
+	}
+
+	this.loadUser = function(err, name){
+		if(err){
+			throw err;
+		}
+		var users = this.userList.filter(function(user){
+			return user.uName.toLowerCase() === name.toLowerCase();
+		});
+		this.currentUser = users.shift();
+		if(this.currentUser !== null){
+			this.i = this.currentUser.currentQ;
+			this.game();
+		}else{
+			console.log("The user doesn't exist.");
+			this.getUser();
+		}
+	}
+
+	this.createUser = function(err, name){
+		if(err){
+			throw err;
+		}
+		this.addUser(name);
+		var users = this.userList.filter(function(user){
+			return user.uName === name;
+		});
+		this.currentUser = users.shift();
+		if(this.currentUser !== null){
+			this.i = this.currentUser.currentQ;
+			this.game();
+		}
 	}
 
 	this.game = function(){
+		console.log(this.userList);
 		if(this.i < this.questionList.length){
-			console.log("You have " + this.totalScore + " points.");
+			console.log("You have " + this.currentUser.uPoints + " points.");
 			this.questionList[this.i].showQuestion();
 			if(this.questionList[this.i].qBonus){
 				console.log("This is a bonus question..." + 
@@ -46,14 +127,20 @@ var Quiz = function(){
 			};				
 			read(options, this.play.bind(this));
 		}else{
-			console.log("You have got a total score of " + this.totalScore + " points.");
+			console.log("You have got a total score of " + this.currentUser.uPoints + " points.");
 			console.log("There are not more questions...");
 		}
 	}
 
 	this.addQuestion = function(qText, qAnswer, qPoint){
-		question = new Question(qText, qAnswer, this.getLastId(), qPoint);
+		var question = new Question(qText, qAnswer, this.getLastId(), qPoint);
 		this.questionList.push(question);
+	}
+
+	this.addUser = function(name){
+		var user = new User(name, 0, 0);
+		this.userList.push(user);
+		console.log(this.userList);
 	}
 
 	this.getLastId = function(){
@@ -90,10 +177,10 @@ var Quiz = function(){
 				points *= 2;
 				strCorrect += "You get double points.";
 			}
-			this.totalScore += points;
+			this.currentUser.uPoints += points;
 			console.log(strCorrect);
 		}else{
-			this.totalScore -= question.qPoint;
+			this.currentUser.uPoints -= 1;
 			console.log("Your answer is incorrect :(... Try again");
 		}
 		this.game();
@@ -102,14 +189,24 @@ var Quiz = function(){
 }
 
 
+var User = function(name, points, num){
+	this.uName = name;
+	this.uPoints = points;
+	this.currentQ = num;
+
+
+}
+
 quiz = new Quiz();
 
 quiz.addQuestion("How many hearts have an octopus??", "3", 5);
-quiz.addQuestion("Which element is heaviest, Mg or Mn??", "Mn", 6);
-quiz.addQuestion("Who sang 'My Way'??", "Frank Sinatra", 6);
-quiz.addQuestion("Where are the Dolomites??", "Italy", 5);
-quiz.addQuestion("What is the capital of Australia??", "Canberra", 5);
+quiz.addQuestion("Which element is heaviest, Mg or Mn??", "mn", 6);
+quiz.addQuestion("Who sang 'My Way'??", "frank sinatra", 6);
+quiz.addQuestion("Where are the Dolomites??", "italy", 5);
+quiz.addQuestion("What is the capital of Australia??", "canberra", 5);
 quiz.addQuestion("How many months have 31 days??", "7", 3);
-quiz.addQuestion("Who invented the telephone??", "Bell", 5);
+quiz.addQuestion("Who invented the telephone??", "bell", 5);
+
+quiz.addUser("Angel");
 
 quiz.startGame();
